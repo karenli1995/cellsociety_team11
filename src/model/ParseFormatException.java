2a8b7329d8634cdbf016a2f645b583d4958e5d3a
Mike Ma <ym67@duke.edu>
2015-10-04 22:54:35 -0400

    CodeMasterpiece_ym67

---
+++ src/model/ParseFormatException.java
@@ -0,0 +1,14 @@
+package model;
+
+public class ParseFormatException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8116419699112365841L;
+	
+	ParseFormatException(String message){
+		super(message);
+	}
+	
+}
